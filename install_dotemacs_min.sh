4958d3e1c3cc13389983ea4b9423aaffacbe09f4
willnet <netwillnet@gmail.com>
2012-12-30 23:27:31 +0900

    Add install_dotemacs_min.sh

---
+++ install_dotemacs_min.sh
@@ -0,0 +1,2 @@
+wget https://gist.github.com/raw/4412858/14d3ae7a6a5e45719bda3987a076a05c564b5327/dotemacs -O ~/.emacs
+sudo wget https://gist.github.com/raw/4412858/14d3ae7a6a5e45719bda3987a076a05c564b5327/dotemacs -O /root/.emacs
