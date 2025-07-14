01d7202d3c2b9b5934651da2bf8812f8df2b4645
Shinichi Maeshima <netwillnet@gmail.com>
2025-07-14 10:39:43 +0900

    Add gitconfig and update install script to include git configuration

--- install.sh
+++ install.sh
@@ -2,9 +2,18 @@
 
 BASHRC="$HOME/.bashrc"
 ALIAS_SH="$(dirname "$0")/alias.sh"
+GITCONFIG="$HOME/.gitconfig"
+GITCONFIG_SH="$(dirname "$0")/gitconfig"
 
 if [ -f "$ALIAS_SH" ]; then
   if ! grep -q '# alias.sh' "$BASHRC"; then
     cat "$ALIAS_SH" >> "$BASHRC"
+  fi
+fi
+
+if [ -f "$GITCONFIG_SH" ]; then
+  if ! grep -q '# gitconfig' "$GITCONFIG"; then
+    echo "# gitconfig" >> "$GITCONFIG"
+    cat "$GITCONFIG_SH" >> "$GITCONFIG"
   fi
 fi
